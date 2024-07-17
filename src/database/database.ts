0a75a9bb9127e0346c499fc9de212d61729ff7aa
Ailén <128065511+ailenpaez@users.noreply.github.com>
2024-07-17 18:31:35 -0300

    Primeras modificaciones en el model.

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,7 +3,8 @@ import { BASE_URL } from "../constants";
 async function getAllData(ruta: string) {
   const response = await fetch(BASE_URL + ruta);
   const data = await response.json();
-  return data;
+  return data
 }
 
 export { getAllData };
+
--- src/model/model.ts
+++ src/model/model.ts
@@ -1,2 +1,58 @@
+import { getAllData } from "../database/database";
 import { Messages } from "../utils/messages";
 
+class HPModel {
+  async getAllCharacters() {
+    try {
+      const result = await getAllData("characters");
+      const { characters } = result;
+      return { message: Messages.OK, data: result };
+    } catch (error) {
+      return {
+        message: Messages.BAD_REQUEST,
+        error: error.message,
+      };
+    }
+  }
+
+  async getAllBooks() {
+    try {
+      const result = await getAllData("books");
+      const { books } = result;
+      return { message: Messages.OK, data: result };
+    } catch (error) {
+      return {
+        message: Messages.BAD_REQUEST,
+        error: error.message,
+      };
+    }
+  }
+
+  async getAllSpells() {
+    try {
+      const result = await getAllData("spells");
+      const { spells } = result;
+      return { message: Messages.OK, data: result };
+    } catch (error) {
+      return {
+        message: Messages.BAD_REQUEST,
+        error: error.message,
+      };
+    }
+  }
+
+  async getAllMovies() {
+    try {
+      const result = await getAllData("movies");
+      const { movies } = result;
+      return { message: Messages.OK, data: result };
+    } catch (error) {
+      return {
+        message: Messages.BAD_REQUEST,
+        error: error.message,
+      };
+    }
+  }
+}
+
+export { HPModel };
